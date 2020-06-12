5cac9be05e955818f6bf0c30f5a5386afefc2327
DavidHDev <haz.d@outlook.com>
2020-06-12 22:00:59 +0300

    Updates

---
+++ precache-manifest.ca27061e4b2910eaab0b67c0fedfcc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f419737f6c6e2ebfc6b112c4fcc74c7c",
+    "url": "/reactcovid/index.html"
+  },
+  {
+    "revision": "1ee74cc91d6288643bdf",
+    "url": "/reactcovid/static/css/main.1f3447af.chunk.css"
+  },
+  {
+    "revision": "a68561b75717e64f2fde",
+    "url": "/reactcovid/static/js/2.8ec77797.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/reactcovid/static/js/2.8ec77797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee74cc91d6288643bdf",
+    "url": "/reactcovid/static/js/main.5f57091b.chunk.js"
+  },
+  {
+    "revision": "5a42c20f63241c33ec1c",
+    "url": "/reactcovid/static/js/runtime-main.034c4d37.js"
+  }
+]);
